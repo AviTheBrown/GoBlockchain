13a76beb3144bdeb755bd8eaa31edba753c64a1b
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-10-24 03:50:30 -0400

    added test block creation

--- main.go
+++ main.go
@@ -8,4 +8,15 @@ func main() {
 	gensis := blockdata.CreateGenesisBlock()
 	gensis.PrintBlockInfo()
 
+	secondBlock := blockdata.CreateNewBlock(gensis, 2)
+	secondBlock.PrintBlockInfo()
+
+	thirdBlock := blockdata.CreateNewBlock(secondBlock, 3)
+	thirdBlock.PrintBlockInfo()
+
+	fourthBlock := blockdata.CreateNewBlock(thirdBlock, 4)
+	fourthBlock.PrintBlockInfo()
+
+	fithBlock := blockdata.CreateNewBlock(fourthBlock, 5)
+	fithBlock.PrintBlockInfo()
 }
