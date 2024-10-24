c5449b1920ca732b1f22657ec7ad760b2e26201b
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-10-24 02:14:57 -0400

    created genhash func

--- Hashing/hashing.go
+++ Hashing/hashing.go
@@ -1,7 +1,13 @@
 package hashing
 
-import "time"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
 
-func GeneratHash(id int, timestamp time.Time, preHash string) {
-
+func GeneratHash(id int, timestamp int64, preHash string) string {
+	data := fmt.Sprintf("%d%s%s", id, timestamp, preHash)
+	hash := sha256.Sum256([]byte(data))
+	return hex.EncodeToString(hash[:])
 }
