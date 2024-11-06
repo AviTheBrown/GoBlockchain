2a9f2007aec5b4719e8bc3d1db8b41c40805a44e
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-11-06 17:46:00 -0500

    added hashing zero

---
+++ Hashing/hashing.go
@@ -0,0 +1,29 @@
+package hashing
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"log"
+	"math/big"
+	mathRand "math/rand"
+)
+
+func GenerateMagicNum() int32 {
+	max := big.NewInt(int64(mathRand.Int31()))
+	randNum, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		log.Fatalln("there was problem generating a rand number")
+	}
+	if randNum.Cmp(big.NewInt(int64(int32(1<<31-1)))) > 0 {
+		log.Fatalln("generated number is out of int32 range")
+	}
+	return int32(randNum.Int64())
+}
+
+func GeneratHash(id int, timestamp int64, magicNumber int32, preHash string, zeros string) string {
+	data := fmt.Sprintf("%d%d%d%s", id, timestamp, magicNumber, preHash)
+	hash := sha256.Sum256([]byte(data))
+	return zeros + hex.EncodeToString(hash[:])
+}
