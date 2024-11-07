c81938cbc077a7366f13c60c5d590cd48d6e8f18
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2024-11-07 01:12:26 -0500

    Added loop to find a sha256 that starts with the number of zeros thats user defined.

--- block_data/block.go
+++ block_data/block.go
@@ -2,17 +2,31 @@ package blockdata
 
 import (
 	"fmt"
-	"github.com/AviTheBrown/Go_Blockchain/hashing"
+	"log"
+	"strconv"
 	"time"
+
+	hashing "github.com/AviTheBrown/Go_Blockchain/hashing"
 )
 
 type Block struct {
 	Id           int
 	Magic_number int32
 	Time_stamp   int64
-	// Data       string
-	PrevHash string
-	Hash     string
+	PrevHash     string
+	Hash         string
+	Gen_time     time.Duration
+}
+
+func formatDuration(d time.Duration) string {
+	seconds := int(d.Seconds())
+	minutes := seconds / 60
+	remainingSeconds := seconds % 60
+
+	if minutes > 0 {
+		return fmt.Sprintf("%d %d .", minutes, remainingSeconds)
+	}
+	return fmt.Sprintf("%d", remainingSeconds)
 }
 
 func (b Block) GetPrevBlockHash(prevBlock Block) string {
@@ -20,6 +34,7 @@ func (b Block) GetPrevBlockHash(prevBlock Block) string {
 
 }
 func CreateGenesisBlock(zeros string) Block {
+	startTime := time.Now()
 	gBlock := Block{
 		Id:           1,
 		Time_stamp:   time.Now().UTC().UnixNano(),
@@ -27,18 +42,48 @@ func CreateGenesisBlock(zeros string) Block {
 		PrevHash:     "0",
 	}
 
+	for {
+		gBlock.Magic_number = hashing.GenerateMagicNum()
+		hash := hashing.GeneratHash(gBlock.Id, gBlock.Time_stamp, gBlock.Magic_number, gBlock.PrevHash, zeros)
+		intZeros, err := strconv.Atoi(zeros)
+		if err != nil {
+			log.Fatal("Couldnt convert the string `zero` to an int.")
+		}
+		if hashing.ValidateHash(hash, intZeros) {
+			gBlock.Hash = hash
+			break
+		}
+	}
+
 	gBlock.Hash = hashing.GeneratHash(gBlock.Id, gBlock.Time_stamp, gBlock.Magic_number, gBlock.PrevHash, zeros)
+	gBlock.Gen_time = time.Since(startTime)
 	return gBlock
 }
 
 func CreateNewBlock(prevBlock Block, id int, zeros string) Block {
+	startTime := time.Now()
 	block := Block{
 		Id:           id,
 		Time_stamp:   time.Now().UTC().UnixNano(),
 		Magic_number: hashing.GenerateMagicNum(),
 		PrevHash:     prevBlock.Hash,
 	}
+
+	for {
+		block.Magic_number = hashing.GenerateMagicNum()
+		hash := hashing.GeneratHash(block.Id, block.Time_stamp, block.Magic_number, block.PrevHash, zeros)
+		intZeros, err := strconv.Atoi(zeros)
+		if err != nil {
+			log.Fatal("Couldnt convert the string `zero` to an int.")
+		}
+		if hashing.ValidateHash(hash, intZeros) {
+			block.Hash = hash
+			break
+		}
+	}
+
 	block.Hash = hashing.GeneratHash(block.Id, block.Time_stamp, block.Magic_number, block.PrevHash, zeros)
+	block.Gen_time = time.Since(startTime)
 	return block
 }
 
@@ -46,17 +91,18 @@ func (b Block) PrintBlockInfo() {
 	if b.Id == 1 {
 		fmt.Printf("Genesis Block:\n")
 		fmt.Printf("Id: %d\n", b.Id)
-		fmt.Printf("Magic Number: %d\n", b.Magic_number)
 		fmt.Printf("Timestamp: %v\n", b.Time_stamp)
+		fmt.Printf("Magic Number: %d\n", b.Magic_number)
 		fmt.Printf("Hash of the previous block:\n%s\n", b.PrevHash)
 		fmt.Printf("Hash of the block:\n%s\n", b.Hash)
+		fmt.Printf("Block was generating for %s seconds\n", formatDuration(b.Gen_time))
 	} else {
 		fmt.Printf("\nBlock:\n")
 		fmt.Printf("Id: %d\n", b.Id)
-		fmt.Printf("Magic Number: %d\n", b.Magic_number)
 		fmt.Printf("Timestamp: %v\n", b.Time_stamp)
+		fmt.Printf("Magic Number: %d\n", b.Magic_number)
 		fmt.Printf("Hash of the previous block:\n%s\n", b.PrevHash)
 		fmt.Printf("Hash of the block:\n%s\n", b.Hash)
-		// fmt.Printf("Data: %s\n", b.Data)
+		fmt.Printf("Block was generating for %s seconds\n", formatDuration(b.Gen_time))
 	}
 }
